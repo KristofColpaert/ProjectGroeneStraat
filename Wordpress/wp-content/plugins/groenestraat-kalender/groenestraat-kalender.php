39a0619876188dccceaa77a392cc6032f7f39534
Rodric <degrooterodric@outlook.com>
2015-06-01 12:20:59 +0200

    update(kalender)

---
+++ Wordpress/wp-content/plugins/groenestraat-kalender/groenestraat-kalender.php
@@ -0,0 +1,103 @@
+<?php
+	/*
+		Plugin Name: Groenestraat Persoonlijke Kalender
+		Plugin URI: http://www.groenestraat.be
+		Description: Deze plugin voegt een persoonlijke kalender custom post type toe. 
+		Version: 1.0
+		Author: Rodric Degroote, Kristof Colpaert
+		Author URI: http://www.groenestraat.be
+		Text Domain: prowp-plugin
+		License: GPLv2		
+	*/
+	
+	/*
+		Add actions
+	*/
+	
+	add_action('init', 'prowp_register_kalender');
+
+	/*
+		Add shortcode
+	*/
+
+	add_shortcode('personal_calendar','prowpt_kalender');
+		
+	$events = array();
+	function prowpt_kalender()
+	{
+
+		$user_id = get_current_user_id();
+		$meta_key = "_eventCalendar ";
+
+		//ophalen data uit database
+		$value = get_user_meta($user_id, $meta_key);
+
+		//alle events ophalen
+		 	//Eerst de titel uit de get_post();
+			//Dan de meta_data uit de get_meta_data();
+		foreach($value as $val)
+		{
+			$event= get_post($val, OBJECT);
+			$event->eventTime = get_post_meta($event->ID, '_eventTime')[0];
+			$events[] = $event;
+		}
+
+
+		?>
+			<link href='<?php echo plugins_url('fullcalendar.css', __FILE__); ?>' rel='stylesheet' />
+			<link href='<?php echo plugins_url('fullcalendar.print.css', __FILE__); ?>' rel='stylesheet' media='print' />
+
+			<!--<script src='<?php echo plugins_url('lib/moment.min.js', __FILE__); ?>'></script>
+			<script src='<?php echo plugins_url('lib/jquery.min.js', __FILE__); ?>'</script>
+			<script src='<?php echo plugins_url('fullcalendar.min.js', __FILE__); ?>'</script>-->
+
+			<script src='http://fullcalendar.io/js/fullcalendar-2.1.1/lib/moment.min.js'></script>
+			<script src='http://fullcalendar.io/js/fullcalendar-2.1.1/lib/jquery.min.js'></script>
+			<script src="http://fullcalendar.io/js/fullcalendar-2.1.1/lib/jquery-ui.custom.min.js"></script>
+			<script src='http://fullcalendar.io/js/fullcalendar-2.1.1/fullcalendar.min.js'></script>
+			<script>
+
+				$(document).ready(function() {
+					console.log("hier");
+					$('#calendar').fullCalendar({
+						defaultDate: new Date(),
+						editable: true,
+						eventLimit: true, // allow "more" link when too many events
+						events: [
+								<?php 
+									foreach($events as $event)
+									{
+										?>
+											{
+												title: '<?php echo $event->post_title; ?>',
+												start: '<?php echo $event->eventTime; ?>',
+												url: '<?php echo get_permalink($event->ID); ?>'
+											},
+										<?php
+									}
+								?>
+						]
+					});
+					
+				});
+
+			</script>
+			<style>
+
+				body {
+					margin: 40px 10px;
+					padding: 0;
+					font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
+					font-size: 14px;
+				}
+
+				#calendar {
+					max-width: 900px;
+					margin: 0 auto;
+				}
+
+			</style>
+			<div id='calendar'></div>
+		<?php
+	}
+?>
