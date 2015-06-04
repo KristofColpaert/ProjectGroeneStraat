a2c229d1a02a2c356cdb250ff8fe16d93ae03b6a
Koen Van Crombrugge <koenvancrombrugge@hotmail.com>
2015-06-04 15:13:20 +0200

    Added a 404 Not Found Template

---
+++ WordpressTheme/groenestraat/404.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<!-- dit tekstje moet nog vervangen worden door een mooie afbeelding 
+best een plantje dat geen bladeren heeft.. dus iemand die tijd heeft? -->
+<style>
+	.not-found
+	{
+	    text-align: center;
+	    background-color: #69686e;
+	    color:#ffffff;
+	}
+
+	.not-found h1
+	{
+	    font-size:25em;
+	    line-height: 2em;
+	}
+
+	.not-found h2
+	{
+	    font-size:10em;
+	}
+</style>
+<section class="container">
+	<section class="not-found">
+		<h1>404</h1>
+		<h2>Not Found!</h2>
+	</section>
+</section>
+
+<?php get_footer(); ?>
