b5eb312749700988b27e4e80ac89face4811cd77
vincentderidder <vincent.v.deridder@gmail.com>
2015-06-04 09:13:42 +0200

    page.php

--- WordpressTheme/groenestraat/page.php
+++ WordpressTheme/groenestraat/page.php
@@ -6,9 +6,14 @@ get_header();
 		<main id="main" class="site-main" role="main">
 <?php
 while ( have_posts() ) : the_post();
+?><div class="contentwrapper">
+            <h1><?php
 the_title();
-
+?>
+            </h1>
+<?php
 the_content();
+?></div><?php
 endwhile;
 ?>
         </main></div>
