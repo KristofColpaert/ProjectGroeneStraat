7bac3e80c7400b9f00ac959f8b5acf2bc27403e5
Koen Van Crombrugge <koenvancrombrugge@hotmail.com>
2015-06-05 11:42:49 +0200

    Added new 404 Template

--- WordpressTheme/groenestraat/404.php
+++ WordpressTheme/groenestraat/404.php
@@ -8,23 +8,26 @@ best een plantje dat geen bladeren heeft.. dus iemand die tijd heeft? -->
 	    text-align: center;
 	    background-color: #69686e;
 	    color:#ffffff;
+	    width: 100%;
+	    height: auto;
 	}
-
+	.not-found img
+	{
+		width: 20%;
+		height: auto;
+		margin:0 auto;
+	}
 	.not-found h1
 	{
-	    font-size:25em;
+	    font-size:15em;
+	    font-weight: bold;
 	    line-height: 2em;
-	}
-
-	.not-found h2
-	{
-	    font-size:10em;
 	}
 </style>
 <section class="container">
 	<section class="not-found">
-		<h1>404</h1>
-		<h2>Not Found!</h2>
+		<h1>404 NOT FOUND!</h1>
+		<img src="http://images2.wikia.nocookie.net/__cb20130221235215/adventuretimesuperfans/images/e/ed/Render_-_Laugh_Face_Pfftch_Troll_Face_BaixeRenders.png">
 	</section>
 </section>
 
