32873d4f21d0635440958da434d45a4861a565b0
vincentderidder <vincent.v.deridder@gmail.com>
2015-06-09 10:22:19 +0200

    Single project

--- WordpressTheme/groenestraat/single-projecten.php
+++ WordpressTheme/groenestraat/single-projecten.php
@@ -1,24 +1,58 @@
-<?php 
+<?php function getMonth($var)
+	{
+		switch ($var) 
+		{
+		case '01':
+			$string = 'JANUARI';
+			break;
+		case '02':
+			$string = 'FEBRUARI';
+			break;
+		case '03':
+			$string = 'MAART';
+			break;
+		case '04':
+			$string = 'APRIL';
+			break;
+		case '05':
+			$string = 'MEI';
+			break;
+		case '06':
+			$string = 'JUNI';
+			break;
+		case '07':
+			$string = 'JULI';
+			break;
+		case '08':
+			$string = 'AUGUSTUS';
+			break;
+		case '09':
+			$string = 'SEPTEMBER';
+			break;
+		case '10':
+			$string = 'OKTOBER';
+			break;
+		case '11':
+			$string = 'NOVEMBER';
+			break;
+		case '12':
+			$string = 'DECEMBER';
+			break;
+		default:
+			# code...
+			break;
+		}
+		return $string;
+	}
 	get_header(); 
 	global $post;
 ?>
 
-	<section class="project-image">
+	<section class="project-image header-image">
 		<section class="project-info">
 
 			<!-- Kristof zijn toevoegingen -->
-			<?php
-				if(is_user_logged_in() && $post->post_author != get_current_user_id())
-				{
-					?>
-						<form method="POST" id="projectMemberForm">
-							<input id="projectMemberId" name="projectMemberId" type="hidden" value="<?php echo get_current_user_id(); ?>" />
-							<input id="projectMemberProjectId" name="projectMemberProjectId" type="hidden" value="<?php echo $post->ID; ?>" />
-							<input id="projectMemberSubmit" name="projectMemberSubmit" type="submit" value="Inschrijven" />
-						</form>
-					<?php
-				}
-			?>
+			
 		</section>
 	</section>
 
@@ -44,48 +78,11 @@
 		var info = document.getElementsByClassName("project-info")[0];
 		var h1 = document.createElement("h1");
 		h1.innerHTML = "<?php the_title(); ?>";
-		var strong = document.createElement("strong");
-		strong.innerHTML = "&#160; <?php echo $adresInfo ?>";
-
-		h1.appendChild(strong);
 		info.appendChild(h1);
 
 
 		</script>
 
-		<?php
-
-			echo "number = " . $post->ID;
-
-		?>
-
-		<?php
-			$args = array(
-				'posts_per_page' => 9,
-				'post_type' => 'projecten'
-			);
-
-			$my_query = new WP_Query($args);
-			//print_r($my_query);
-
-			while($my_query->have_posts()) : $my_query->the_post();
-				/*
-				$meta = get_post_meta($post->ID);
-				$projectStreet = $meta['_projectStreet'][0];
-				$projectCity = $meta['_projectCity'][0];
-				$projectZipcode = $meta['_projectZipcode'][0];
-				*/
-			?>
-
-				<section class="item">
-					<h1><?php echo the_title(); ?></h1>
-
-				</section>
-
-			<?php
-			endwhile;
-		?>
-		
 		<!--
 		<?php the_post_thumbnail(); ?>
 		<h1><?php the_title(); ?></h1>
@@ -95,8 +92,131 @@
 		<p>Postcode: <?php echo $projectZipcode; ?></p>
 		-->
 
+		<?php 
+            $args = array(
+				'posts_per_page' => 9,
+				'post_type' => array('zoekertjes','events','post'),
+                'meta_key' => '_parentProjectId',
+                'meta_value'=>$id,
+                'meta_compare'=>'='
+                
+			     );
+            $my_query = new WP_Query($args);
+
+			while($my_query->have_posts()) : $my_query->the_post();
+                switch($post->post_type){
+                    case "events":{
+                        $meta = get_post_meta($post->ID);
+                        $eventTime = $meta['_eventTime'][0];
+                        $eventLocation = $meta['_eventLocation'][0];
+                        $eventEndTime = $meta['_eventEndTime'][0];
+
+                        $day = explode("/", $eventTime)[1];
+                        $monthNumber = explode("/", $eventTime)[0];
+                        $month = getMonth($monthNumber);
+		?>
+
+               
+                            <section class="list-item normalize-text <?php echo $post->post_type;?>">
+                                <section class="event-calendar">
+                                    <h1><?php echo $day; ?></h1>
+                                    <h2><?php echo $month; ?></h2>
+                                </section>
+                                <section class="event-content">
+                                    <h1><?php the_title(); ?></h1>
+                                    <p><strong>Tijdstip: </strong><?php echo $eventTime . " - " . $eventEndTime; ?></p>
+                                    <p><strong>Locatie: </strong><?php echo $eventLocation; ?></p>
+                                    <p><strong>Meer info: </strong><?php echo excerpt(50); ?></p>
+                                     
+                                </section>
+                                <a class="view-item" href="<?php the_permalink(); ?>">Bekijk event</a>
+                            </section>
+                        
+
+		<?php
+                    }
+                    break;
+                    case "zoekertjes":{
+                        $meta = get_post_meta($post->ID);
+                        $adLocation = $meta['_adLocation'][0];
+                        $adPrice = $meta['_adPrice'][0];
+                    ?>
+
+                   
+                        <section class="list-item normalize-text <?php echo $post->post_type;?>">
+                            <h1><?php the_title(); ?></h1>
+                            <p><strong>Beschrijving: </strong><?php the_content(); ?></p>
+                            <p><strong>Locatie: </strong><?php echo $adLocation; ?></p>
+                            <p><strong>Prijs: </strong><?php echo $adPrice; ?></p>
+                             <a class="view-item" href="<?php the_permalink(); ?>">Bekijk zoekertje</a>
+                        </section>
+                    
+
+		<?php
+                    
+                    }break;
+                    case "post":{
+                        ?>
+                        <section class="list-item normalize-text <?php echo $post->post_type; ?>">
+					<h1><?php echo the_title();?></h1>
+                    <?php echo the_excerpt();
+?>				 <a class="view-item" href="<?php the_permalink(); ?>">Lees meer</a></section><?php
+                    }break;
+                    default: break;
+                }
+			?>
+
+				
+
+			<?php
+			endwhile;
+            wp_reset_postdata();
+            ?>
 		</section>
-		<section class="sidebar"></section>
+		<section class="sidebar">
+            <section class="search normalize-text">
+                <?php
+				if(is_user_logged_in() && $post->post_author != get_current_user_id())
+				{
+					?>
+						<form method="POST" id="projectMemberForm">
+							<input id="projectMemberId" name="projectMemberId" type="hidden" value="<?php echo get_current_user_id(); ?>" />
+							<input id="projectMemberProjectId" name="projectMemberProjectId" type="hidden" value="<?php echo $post->ID; ?>" />
+							<input id="projectMemberSubmit" name="projectMemberSubmit" type="submit" value="Inschrijven" class="form-button" />
+						</form>
+                    <hr/>
+					<?php
+				}
+
+			?>
+                
+                <section class="form-line">
+                    <input class="checkbox js-switch" id="zoekertjes" onchange="apply_filter('z')" type="checkbox" name="zoekertjes" value="1" checked/>
+                    <label class="checkbox-label">Toon zoekertjes</label>
+                </section>
+                <section class="form-line">
+                    <input class="checkbox js-switch" id="events" onchange="apply_filter('e')" type="checkbox" name="events" value="1" checked/>
+                    <label class="checkbox-label">Toon events</label> 
+                </section>
+                <section class="form-line">
+                    <input class="checkbox js-switch" id="post" onchange="apply_filter('a')" type="checkbox" name="artikels" value="1" checked/>
+                    <label class="checkbox-label">Toon artikels</label> 
+                </section>
+                
+                <hr />
+                
+                <section class="form-line">
+                    <h3>Beschrijving</h3>
+                        <?php the_content(); ?>
+                </section>
+                
+                <section class="form-line">
+        
+                   <a href="<?php echo get_site_url() ?>/projectleden?project=<?php echo $post->ID; ?>">Leden</a>
+                </section>
+            </section>
+            
+        </section>
 
 		<?php
 	endwhile;
@@ -104,7 +224,48 @@
 
 	</section>
 	<section class="clear"></section>
-	
+	<!----switches--->
+<script>
+    var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+    elems.forEach(function(html) {
+        var switchery = new Switchery(html, {color:'#00cd00', size:'small'});
+    });
+    $('.events').css('height', $('.events').height());
+    function apply_filter(type){
+        console.log(type);
+        var el;
+        if(type=='z'){
+            el ="zoekertjes";
+        }
+        if(type=='e'){
+            el="events";
+        }
+        if(type=='a'){
+            el="post";
+        }
+        if($("#"+el).prop("checked")){
+                
+                $('.'+el).animate(
+	    		{
+				    height: 'toggle',
+                    padding: '2%',
+                    margin:'5% 5% 0 5%',
+				}, 500);
+            }
+            else {
+                $('.'+el).animate(
+	    		{
+                    padding:'0% 2%',
+				    height: 'toggle',
+                    margin:'0% 5% -5% 5%',
+				}, 500);
+            }
+    }
+    
+    
+    
+        
+</script>
 	<?php
 	get_footer();
 ?>
