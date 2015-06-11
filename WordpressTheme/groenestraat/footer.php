7752a428f5cc72360d6fdcb9c0231d71f9e999e9
Koen Van Crombrugge <koenvancrombrugge@hotmail.com>
2015-06-11 12:08:46 +0200

    Updated footer

--- WordpressTheme/groenestraat/footer.php
+++ WordpressTheme/groenestraat/footer.php
@@ -1,77 +1,43 @@
     <footer>
         <section class="footer-partners">
             <div id="partners-slider">
-                <div class="item"><img src="<?php bloginfo('template_directory'); ?>/img/partner-example.jpg" alt="partner"></div>
-                <div class="item"><img src="<?php bloginfo('template_directory'); ?>/img/partner-example.jpg" alt="partner"></div>
-                <div class="item"><img src="<?php bloginfo('template_directory'); ?>/img/partner-example.jpg" alt="partner"></div>
-                <div class="item"><img src="<?php bloginfo('template_directory'); ?>/img/partner-example.jpg" alt="partner"></div>
-                <div class="item"><img src="<?php bloginfo('template_directory'); ?>/img/partner-example.jpg" alt="partner"></div>
-                <div class="item"><img src="<?php bloginfo('template_directory'); ?>/img/partner-example.jpg" alt="partner"></div>
-                <div class="item"><img src="<?php bloginfo('template_directory'); ?>/img/partner-example.jpg" alt="partner"></div>
-                <div class="item"><img src="<?php bloginfo('template_directory'); ?>/img/partner-example.jpg" alt="partner"></div>
-                <div class="item"><img src="<?php bloginfo('template_directory'); ?>/img/partner-example.jpg" alt="partner"></div>
-                <div class="item"><img src="<?php bloginfo('template_directory'); ?>/img/partner-example.jpg" alt="partner"></div>
-                <div class="item"><img src="<?php bloginfo('template_directory'); ?>/img/partner-example.jpg" alt="partner"></div>
-                <div class="item"><img src="<?php bloginfo('template_directory'); ?>/img/partner-example.jpg" alt="partner"></div>
-                <div class="item"><img src="<?php bloginfo('template_directory'); ?>/img/partner-example.jpg" alt="partner"></div>
+                <?php
+                    global $wpdb;
+                    $results = $wpdb->get_results( "SELECT * FROM wp_partners", ARRAY_A);
+                    foreach($results as $result)
+                    {
+                        ?>
+                            <div class="item">
+                                <a target="_blank" href="<?php echo $result["URL"]; ?>">
+                                    <div style="background-image:url('<?php echo $result["URLImage"]; ?>');background-size:cover;width:auto;height:185px;-webkit-filter: grayscale(1);filter: grayscale(1);"></div>
+                                </a>
+                            </div>
+                        <?php
+                    }
+                ?>
             </div>       
         </section>
         <section class="footer-content">
             <section class="footer-sitemap">
+                <h1>Sitemap</h1><br/><br/>
                 <ul>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
-                    <li><a href="#">sitemap item link</a></li>
+                    <?php
+                        $pages = get_pages(); 
+                        foreach($pages as $page) 
+                        {
+                            echo '<li><a href="' . get_page_link( $page->ID ) . '">' . $page->post_title . '</a></li>';
+                        }
+                    ?>
                 </ul>
             </section>
             <section class="footer-logo">
                 <img src="<?php bloginfo('template_directory'); ?>/img/logo_large.png" width="500" height="500" alt="" title="" />
+                <p>© Copyright 2015. Meer info komt hier terecht.</p>
             </section>
             <section class="footer-links">
-                <ul>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                    <li><a href="#">gebruikersvoorwaarden</a></li>
-                </ul>
+            <h1>Social</h1><br/><br/>
+                <a class="twitter-timeline"  href="https://twitter.com/hashtag/groenestraat" data-widget-id="608932741124648960">#groenestraat Tweets</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
             </section>
             <section class="clear"></section>
         </section>
