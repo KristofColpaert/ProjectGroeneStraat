d72962360432ca282fa91003954c6543404f3b19
kriscolp <kriscolp@gmail.com>
2015-06-12 10:25:13 +0200

    Bug fix

--- Plugins source/groenestraat-projectArticleManagement/groenestraat-projectArticleManagement.php
+++ Plugins source/groenestraat-projectArticleManagement/groenestraat-projectArticleManagement.php
@@ -50,61 +50,58 @@
 
 	function show_pending_articles()
 	{
-		if(is_user_logged_int())
+		$current_user = wp_get_current_user();
+		$args = array(
+			'orderby' => 'title',
+			'order' => 'ASC',
+			'numberposts' => -1,
+			'post_type' => 'projecten',
+			'author' => $current_user->ID
+		);
+		$projects = get_posts($args);
+
+		?>
+			<section id="projectArticleMainContainer">
+		<?php
+
+		foreach($projects as $project)
 		{
-			$current_user = wp_get_current_user();
 			$args = array(
 				'orderby' => 'title',
 				'order' => 'ASC',
 				'numberposts' => -1,
-				'post_type' => 'projecten',
-				'author' => $current_user->ID
+				'post_type' => 'post',
+				'post_status' => 'pending',
+				'meta_key' => '_parentProjectId',
+				'meta_value' => $project->ID
 			);
-			$projects = get_posts($args);
 
-			?>
-				<section id="projectArticleMainContainer">
-			<?php
+			$posts = get_posts($args);
+			$counter2 = 1;
 
-			foreach($projects as $project)
+			foreach($posts as $post)
 			{
-				$args = array(
-					'orderby' => 'title',
-					'order' => 'ASC',
-					'numberposts' => -1,
-					'post_type' => 'post',
-					'post_status' => 'pending',
-					'meta_key' => '_parentProjectId',
-					'meta_value' => $project->ID
-				);
-
-				$posts = get_posts($args);
-				$counter2 = 1;
-
-				foreach($posts as $post)
-				{
-					?>
-						<article class="box" id="projectArticleContainer<?php echo $post->ID; ?>">
-	                        <section >
-	                            <h2 class="normalize-text"><?php echo $post->post_title; ?></h2>
-							    <p class="normalize-text"><?php echo $post->post_content; ?></p>
-	                        </section>
+				?>
+					<article class="box" id="projectArticleContainer<?php echo $post->ID; ?>">
+	                    <section >
+	                        <h2 class="normalize-text"><?php echo $post->post_title; ?></h2>
+						    <p class="normalize-text"><?php echo $post->post_content; ?></p>
+	                    </section>
 							
-							<form action="<?php $_SERVER['REQUEST_URI']; ?>" method="POST">
-								<input type="button" id="projectArticleSubmit<?php echo $post->ID; ?>" data="<?php echo $post->ID; ?>" name="projectArticleSubmit" class="projectArticleSubmit confirm-button-green" value="Accepteren" />
-								<input type="button" id="projectArticleDelete<?php echo $post->ID; ?>" data="<?php echo $post->ID; ?>" name="projectArticleDelete" class="projectArticleDelete confirm-button" value="Verwijderen" />
-							</form>
+						<form action="<?php $_SERVER['REQUEST_URI']; ?>" method="POST">
+							<input type="button" id="projectArticleSubmit<?php echo $post->ID; ?>" data="<?php echo $post->ID; ?>" name="projectArticleSubmit" class="projectArticleSubmit confirm-button-green" value="Accepteren" />
+							<input type="button" id="projectArticleDelete<?php echo $post->ID; ?>" data="<?php echo $post->ID; ?>" name="projectArticleDelete" class="projectArticleDelete confirm-button" value="Verwijderen" />
+						</form>
 	                        
-						</article>
+					</article>
 	               
-					<?php
-				}
+				<?php
 			}
-			?>
-	                <section class="clear"></section>
-				</section>
-			<?php
 		}
+		?>
+	            <section class="clear"></section>
+			</section>
+		<?php
 	}
 
 	function add_project_article()
