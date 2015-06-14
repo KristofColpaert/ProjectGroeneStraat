a64ee56c2500348d7afe25c06ce58c5f1474ff96
Koen Van Crombrugge <koenvancrombrugge@hotmail.com>
2015-06-14 15:03:03 +0200

    Fix single artikels

--- WordpressTheme/groenestraat/single.php
+++ WordpressTheme/groenestraat/single.php
@@ -1,29 +1,23 @@
 <?php get_header();
 	
 	global $post;
-
 	?>
 
 	<section class="container">
 		<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		<div class="contentwrapper">
-
-		<style>
-			
-		</style>
-
 	<?php
 
 	while(have_posts()) : the_post();
 		?>
 			<h1><?php the_title(); ?></h1><br/>
-			<p><strong>Beschrijving:</strong><br/><?php echo the_content(); ?></p>
-			<br/>
+			<p><br/><?php echo the_content(); ?></p>
+			<br/><br/><br/>
 			<?php if(has_post_thumbnail($post->ID)) { ?>
 				<?php echo get_the_post_thumbnail(); ?>
 			<?php } ?>
-			<br/><br/>
+			<br/><br/><br/>
 			<?php comments_template(); ?>
 			<?php endwhile; ?>
 				</div>
@@ -43,8 +37,9 @@
 	?>
 	<script>
 
+		//$('.vcard').prepend('<p style="float:right;">' + '#datum#' + '<p>');
 		$('.comment-reply-link').removeClass('comment-reply-link').addClass('form-button');
-		$('.form-button').css({'float:':'left', 'width':'30%', 'margin':'15px 0 0 0', 'font-size' : '1.5em', 'text-align': 'center'});
+		$('.form-button').css({'float:':'left', 'width':'150px', 'margin':'15px 0 0 0', 'font-size' : '1.5em', 'text-align': 'center'});
 
 		var form = document.getElementById('commentform');
 		form.getElementsByTagName('p')[0].style.display = 'none';
@@ -54,7 +49,6 @@
 
 		document.getElementById('comment').setAttribute('class', 'input');
 		document.getElementById('comment').setAttribute('placeholder', 'Comment');
-
 		document.getElementById('submit').setAttribute('class', 'form-button');
 		document.getElementById('submit').setAttribute('style', 'float:left');
 		document.getElementById('submit').setAttribute('value', 'plaats reactie');
