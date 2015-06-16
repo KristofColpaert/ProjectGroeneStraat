86446c19a4785bbf9591835deffd6a6d9ac536bd
Rodric <degrooterodric@outlook.com>
2015-06-16 14:28:25 +0200

    APIKey

--- Plugins source/groenestraat-apikey/groenestraat-apikey.php
+++ Plugins source/groenestraat-apikey/groenestraat-apikey.php
@@ -4,7 +4,7 @@
 		Plugin URI: http://www.groenestraat.be
 		Description: Deze plugin zorgt ervoor dat u een API key kunt meegeven van Google, Facebook en Twitter 
 		Version: 1.0
-		Author: Rodric Degroote
+		Author: Rodric Degroote, Kristof Colpaert
 		Author URI: http://www.groenestraat.be
 		Text Domain: prowp-plugin
 		License: GPLv2		
@@ -14,19 +14,13 @@
 	{
 		if(isset($_POST["ApplicationID"]) && !empty($_POST["ApplicationID"]))
 		{
-			print "test";
-
 			$id = $_POST["ApplicationID"];
 			$name = "_applicationID";
-
-			$option = get_option($name);
 			
 			if(get_option($name)) {
 				update_option($name, $id);
-				print "update";
 			} else {
 				add_option($name, $id);
-				print "add";
 			}
 		}
 	}
@@ -43,12 +37,29 @@
 	function add_keys_metaboxes(){
 		?>
 		<div class="wrap" id="wp-media-grid">
-		<h2>API Key Google</h2>
-		<form class="createForm" method="post" action="<?php echo get_permalink(); ?>" enctype="multipart/form-data">
-				<h3 class="hndle ui-sortable-handle">Application Key: </h3>
-				<input type="text" name="ApplicationID" placeholder="Application Key" /><br />
-				<input type="submit" value="Opslaan" name="Opslaan" class="button button-primary button-large"/>
-		</form>
+			<h2>Nieuwe API key</h2>
+				<form class="createForm" method="post" action="<?php echo get_permalink(); ?>" enctype="multipart/form-data">
+					<p><b>Application Key: </b></p>
+					<input type="text" name="ApplicationID" placeholder="Application key" /><br />
+					<input type="submit" value="Opslaan" name="Opslaan" style="margin-top: 10px" class="button button-primary"/>
+				</form>
+		</div>
+		<div class="wrap" id="wp-media-grid">
+			<h2>Huidige API key</h2>
+			<p>
+				<?php 
+					$name = "_applicationID";
+					if(get_option($name)) {
+						?>
+							<p><b>Application Key: </b><?php echo get_option($name); ?></p>
+						<?php
+					} else {
+						?>
+							<p style="color: red">Er werd nog geen API Key meegegeven.</p>
+						<?php
+					}
+				?>
+			</p>
 		</div>
 		<?php
 	}
